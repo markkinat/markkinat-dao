48965957b81ca0b94574cd26a5e35702b1e23efb
pheranmie <okekunletobi@gmail.com>
2024-04-23 12:15:24 +0100

    wrote more stuff innft contract

--- src/contracts/MarkkinatNFT.sol
+++ src/contracts/MarkkinatNFT.sol
@@ -2,24 +2,133 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MarkkinatNFT is Ownable, ERC721URIStorage {
-    uint256 private tokenId = 21;
+    /**
+     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
+    string _baseTokenURI;
+    uint256 private tokenIds;
+
+    //  _price is the price of one Markkinat Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // max number of MarkkinatDevs
+    uint256 public maxTokenIds = 100;
+
+    // boolean to keep track of whether presale started or not
+    bool private nftReserved;
+
+    // boolean to keep track of whether presale started or not
+    bool public presaleStarted;
+
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
 
     constructor(
+        string memory baseURI,
         address initialOwner
     ) ERC721("Markkinat DAO", "MKNDAO") Ownable(initialOwner) {}
 
     bool public saleIsActive = false;
 
-    function flipSaleState() public onlyOwner {
-        saleIsActive = !saleIsActive;
+    /**
+     * @dev startPresale starts a presale for the whitelisted addresses
+     */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        // Set presaleEnded time as current timestamp + 5 minutes
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
+        presaleEnded = block.timestamp + 30 minutes;
     }
 
-    function mintTo(string memory _baseUri) external {
-        require(saleIsActive, "Sale must be active to mint");
-        _mint(msg.sender, tokenId);
-        _setTokenURI(tokenId, _baseUri);
-        tokenId++;
+    /**
+     * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
+     */
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(nftReserved, "Markkinat not reserved");
+        tokenIds = 20;
+        require(
+            presaleStarted && block.timestamp < presaleEnded,
+            "Presale is not running"
+        );
+        require(
+            tokenIds < maxTokenIds,
+            "Exceeded maximum Markkinat Collection supply"
+        );
+        // require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        //_safeMint is a safer version of the _mint function as it ensures that
+        // if the address being minted to is a contract, then it knows how to deal with ERC721 tokens
+        // If the address being minted to is not a contract, it works the same way as _mint
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+     * Set some Markkinat aside
+     */
+    function reserveMarkkinat() public onlyOwner {
+        uint i;
+        for (i = 0; i < 21; i++) {
+            _safeMint(msg.sender, i);
+        }
+        nftReserved = true;
+    }
+
+    /**
+     * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+     */
+    function mint() public payable onlyWhenNotPaused {
+        require(nftReserved, "Markkinat not reserved");
+        require(
+            presaleStarted && block.timestamp >= presaleEnded,
+            "Presale has not ended yet"
+        );
+        require(tokenIds < maxTokenIds, "Exceed maximum Markkinat Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+     * returned an empty string for the baseURI
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /**
+     * @dev setPaused makes the contract paused or unpaused
+     */
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    /**
+     * @dev withdraw sends all the ether in the contract
+     * to the owner of the contract
+     */
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
+    modifier onlyWhenNotPaused() {
+        require(!_paused, "Contract currently paused");
+        _;
     }
 }
