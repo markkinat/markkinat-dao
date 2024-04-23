8d83c9b537c7bfb28b4679176885f3f34dac6cd8
pheranmie <okekunletobi@gmail.com>
2024-04-23 11:32:57 +0100

    added necessary files

--- src/contracts/MarkkinatNFT.sol
+++ src/contracts/MarkkinatNFT.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MarkinatNFT is ERC721URIStorage {
+contract MarkkinatNFT is ERC721URIStorage {
 
     uint256 private tokenId = 21;
-    constructor() ERC721("MarkinatNFT", "MKN"){}
+    constructor() ERC721("Markkinat DAO", "MKNDAO"){}
 
     function mintTo(address _tokenId, string memory _baseUri) external{
         _mint(msg.sender, _tokenId);
