118eeeedca6fa9a37deda067f4620e5e6c849d74
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-04-23 16:09:11 +0100

    voteOnproposal done

--- src/contracts/MarkkinatGovernance.sol
+++ src/contracts/MarkkinatGovernance.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.20;
 
 import "src/interfaces/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract MarkkinatGovernance is Ownable {
+contract MarkkinatGovernance is Ownable, ReentrancyGuard {
     struct Proposal {
         uint256 proposalId;
         string name;
@@ -31,11 +32,12 @@ contract MarkkinatGovernance is Ownable {
         For
     }
 
-    uint16 private quorum;
+    uint16 public quorum;
     mapping(uint256 => Proposal) private proposals;
     mapping(address => Delegate) private delegate;
     uint256 private proposalCount;
     IERC721 private markkinatNFT;
+    uint256 private idsAllowedToVoted;
 
     constructor(
         address nftAddress,
@@ -58,27 +60,84 @@ contract MarkkinatGovernance is Ownable {
         _;
     }
 
+    // Create a modifier which only allows a function to be
+    // called if the given proposal's deadline has not been exceeded yet
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadLine > block.timestamp,
+            "DEADLINE_EXCEEDED"
+        );
+        _;
+    }
+
+    // Create a modifier which only allows a function to be
+    // called if the given proposals' deadline HAS been exceeded
+    // and if the proposal has not yet been executed
+    modifier inactiveProposalOnly(uint256 proposalId) {
+        require(
+            proposals[proposalId].deadLine <= block.timestamp,
+            "DEADLINE_NOT_EXCEEDED"
+        );
+        require(
+            proposals[proposalId].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
+        _;
+    }
+
+    modifier canParticipateInProposal(uint256 _tokenId){
+//        require(markkinatNFT.ownerOf(_tokenId) == msg.sender, "Not owner of this asset");
+        require(_tokenId <= idsAllowedToVoted, "Provided asset not allowed to participate in proposal or Vote");
+        _;
+    }
+
     function createProposal(
         string memory _name,
         uint256 _deadLine,
         string memory desc
     ) external onlyNftHolder {
+        require(bytes(_name).length > 0, "Proposal name cannot be empty");
+        require(bytes(desc).length > 0, "Proposal description cannot be empty");
         uint256 proposalId = proposalCount++;
         Proposal storage proposal = proposals[proposalId];
         proposal.proposalId = proposalId;
+        proposal.creator = msg.sender;
         proposal.name = _name;
         proposal.description = desc;
-        proposal.deadLine = _deadLine;
+        proposal.deadLine = block.timestamp + _deadLine;
     }
 
     function voteOnProposal(
         uint256 proposalId,
-        VoterDecision decision
-    ) external {}
+        VoterDecision decision,
+        uint256 _tokenId
+    ) external activeProposalOnly(proposalId) nonReentrant canParticipateInProposal(_tokenId){
+        Proposal storage proposal = proposals[proposalId];
+        require(proposal.voter[_tokenId] == false, "ALREADY_VOTED");
+        if(decision == VoterDecision.For){
+            proposal.forProposal++;
+        }
+        else if (decision == VoterDecision.Against){
+            proposal.againstProposal++;
+        }
+        else {
+            proposal.abstainProposal++;
+        }
+        proposal.votes++;
+        proposal.voter[_tokenId] = true;
+    }
 
-    function executeProposal() external {}
+    function executeProposal(uint256 proposalId) external {
 
-    function delegateVotingPower(address _delegate, uint256 _tokenId) external {}
+    }
+
+    function delegateVotingPower(address _delegate, uint256 _tokenId) external {
+
+    }
+
+    function updateAllowedIdToVote(uint256 num) external onlyOwner{
+        idsAllowedToVoted = num;
+    }
 
     function updateQuorum(uint16 _quorum) external onlyOwner {
         quorum = _quorum;
@@ -95,28 +154,4 @@ contract MarkkinatGovernance is Ownable {
 
     fallback() external payable {}
 
-    // Create a modifier which only allows a function to be
-    // called if the given proposal's deadline has not been exceeded yet
-    modifier activeProposalOnly(uint256 proposalIndex) {
-        require(
-            proposals[proposalIndex].deadLine > block.timestamp,
-            "DEADLINE_EXCEEDED"
-        );
-        _;
-    }
-
-    // Create a modifier which only allows a function to be
-    // called if the given proposals' deadline HAS been exceeded
-    // and if the proposal has not yet been executed
-    modifier inactiveProposalOnly(uint256 proposalIndex) {
-        require(
-            proposals[proposalIndex].deadLine <= block.timestamp,
-            "DEADLINE_NOT_EXCEEDED"
-        );
-        require(
-            proposals[proposalIndex].executed == false,
-            "PROPOSAL_ALREADY_EXECUTED"
-        );
-        _;
-    }
 }
