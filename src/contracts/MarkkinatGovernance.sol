2acfa74d43ec516f471d3c94c5941f8498339a17
Okekunle  Oluwatobiloba Samuel <47566668+Oluwatobilobaoke@users.noreply.github.com>
2024-04-23 11:11:02 +0100

    Revert "updated"

--- src/contracts/MarkkinatGovernance.sol
+++ src/contracts/MarkkinatGovernance.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "src/interfaces/IERC721.sol";
 
 contract MarkkinatGovernance {
-
     struct Proposal {
         uint256 proposalId;
         string name;
@@ -37,7 +36,6 @@ contract MarkkinatGovernance {
     mapping(address => Delegate) private delegate;
     uint256 private proposalCount;
     IERC721 private nftContract;
-    mapping (uint256 => bool) private activeProps;
 
     constructor(address nftAddress, uint16 _quorum) payable {
         quorum = _quorum;
@@ -57,13 +55,8 @@ contract MarkkinatGovernance {
         _;
     }
 
-    modifier activeProposal(uint proposalId) {
-        require(activeProps[proposalId], "Proposal not active");
-        _;
-    }
-
     function createProposal(string memory _name, uint256 _deadLine, string memory desc) external onlyNftHolder {
-        uint256 proposalId = ++proposalCount;
+        uint256 proposalId = proposalCount++;
         Proposal storage proposal = proposals[proposalId];
         proposal.proposalId = proposalId;
         proposal.name = _name;
@@ -71,21 +64,8 @@ contract MarkkinatGovernance {
         proposal.deadLine =  _deadLine;
     }
 
-    function voteOnProposal(uint256 proposalId, VoterDecision decision) external activeProposal(proposalId){
-        Proposal storage proposal = proposals[proposalId];
-        if(block.timestamp <= proposal.deadLine){
-            if(decision == VoterDecision.For){
-                proposal.forProposal++;
-            } else if (decision == VoterDecision.Against){
-                proposal.againstProposal++;
-            }
-            else proposal.abstainProposal++;
-            proposal.votes++;
-        }
+    function voteOnProposal(uint256 proposalId, VoterDecision decision) external {
 
-        if(block.timestamp > proposal.deadLine){
-            activeProps[proposalId] = true;
-        }
     }
 
     function executeProposal() external {}
